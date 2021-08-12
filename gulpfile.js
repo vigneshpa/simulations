80093f0866a4017a9653d95262bfc49d42be03c2
vignesh <vignesh-p@outlook.com>
2021-08-12 14:07:04 +0530

    populating html from javascript

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,10 @@ const { readdirSync, statSync, stat } = require('fs');
 const { join } = require('path');
 const { task } = require('gulp');
 const webpack = require('webpack');
+const DevServer = require('webpack-dev-server');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 const srcDir = join(__dirname, 'src');
 const distDir = join(__dirname, 'docs');
@@ -37,6 +39,9 @@ const configs = sims.map(sim => ({
     ],
   },
   resolve: { extensions: ['.js', '.jsm', '.ts', '.json'] },
+  optimization: {
+    minimizer: [`...`, new CssMinimizerPlugin()],
+  },
   plugins: [
     new MiniCssExtractPlugin({ filename: 'assets/style.css' }),
     new HtmlWebpackPlugin({
@@ -51,12 +56,14 @@ const configs = sims.map(sim => ({
     assetModuleFilename: 'assets/[name][ext][query]',
     path: join(distDir, sim),
     clean: true,
+    publicPath: './',
   },
   devtool: 'source-map',
   mode: 'production',
 }));
 
 sims.forEach((sim, index) => task(sim, cb => webpackCompile(configs[index], cb)));
+sims.forEach((sim, index) => task('dev-' + sim, cb => webpackWatch(configs[index], cb)));
 const buildAll = cb => webpackCompile(configs, cb);
 exports.default = buildAll;
 
@@ -66,4 +73,11 @@ function webpackCompile(config, cb) {
     if (err) throw err;
     cb();
   });
+}
+function webpackWatch(config, cb) {
+  // if (config.devtool) config.devtool = 'eval-source-map';  // Enable this for faster dev builds but css sourcemaps will not work
+  if (config.mode) config.mode = 'development';
+  new DevServer(webpack(config)).listen(3000, '0.0.0.0', err =>
+    err ? console.error(err) : console.log('Development server is running at port 3000')
+  );
 }
--- src/inductor/index.ts
+++ src/inductor/index.ts
@@ -1,5 +1,6 @@
-import './style.scss'; // importing style sheet
+import './style.css'; // importing style sheet
 
+// Constants
 const inductance = 10; // in Henries(H)
 const resistance = 5; // in Ohms(Ω)
 const sineWaveFrequency = 0.2; // in Hertz (Hz)
@@ -7,37 +8,24 @@ const sineWaveAngularVelocity = 2 * Math.PI * sineWaveFrequency; // in rad/sec a
 const voltageMax = 10;
 const currentMax = 2;
 const initialInductorCurrent = -voltageMax / (inductance * sineWaveAngularVelocity); // as -I₀=-E₀/Lω
-
 const angularVelocityCurrentRatio = 1;
+const integrateResistorCurrent = (delta: number) => voltage / resistance; // as V=iR
+const integrateInductorCurrent = (delta: number) => (currentIntegral += (delta * voltage) / inductance); // as V=-L(di/dt)
+const integrateResIndCurrent = (delta: number) => (currentIntegral += (delta * (voltage - currentIntegral * resistance)) / inductance); // as V=-L(di/dt)+iR
 
+// Variables
+let integrateCurrent = integrateResistorCurrent;
 let voltage = 0;
 let rps = 0;
-
-const voltageInp = document.getElementsByName('voltage')[0] as HTMLInputElement;
-voltageInp.max = voltageMax as unknown as string;
-voltageInp.min = -voltageMax as unknown as string;
-const volEl = document.getElementById('voltageValue')!;
-window.onVoltage = (val: string) => {
-  voltage = +val;
-  volEl.innerText = (+val).toFixed(1);
-};
-const curEl = document.getElementById('currentValue')!;
-
-const curPgr = document.getElementById('currentPgr') as HTMLProgressElement;
-curPgr.max = currentMax * 2;
+let wheelR: number = 0;
+let currentIntegral: number = 0;
+let loadtype: 'Resistive' | 'Inductive' | 'ResInd' | 'InductiveCorrected' = 'Resistive';
+let generateSine: boolean = false;
+let generatorStartTS: number | null = null;
+let previousTS: number = 0;
 
 const wheelEl = document.getElementById('flywheel')!;
-let wheelR: number = 0;
-
-let loadtype: Parameters<Window['onCircuitType']>[0] = 'Resistive';
-
-// Current integral for integration
-let currentIntegral: number = 0;
-
-// Sine function generator
-let generatorStartTS: number | null = null;
-let generateSine: boolean = false;
-window.onGenerateSine = checked => {
+const changeGenerateSine = (checked: boolean) => {
   generatorStartTS = null;
   currentIntegral = 0;
   rps = 0;
@@ -45,21 +33,10 @@ window.onGenerateSine = checked => {
   if (generateSine && loadtype === 'InductiveCorrected') currentIntegral = initialInductorCurrent;
 };
 
-// Resistor function
-const integrateResistorCurrent = (delta: number) => voltage / resistance; // as V=iR
-
-// Inductor function
-const integrateInductorCurrent = (delta: number) => (currentIntegral += (delta * voltage) / inductance); // as V=-L(di/dt)
-
-// ResInd function
-const integrateResIndCurrent = (delta: number) => (currentIntegral += (delta * (voltage - currentIntegral * resistance)) / inductance); // as V=-L(di/dt)+iR
-
-let integrateCurrent = integrateResistorCurrent;
-
-window.onCircuitType = loadt => {
+const changeCircuitType = (loadt: typeof loadtype) => {
   currentIntegral = 0;
   loadtype = loadt;
-  if (generateSine) window.onGenerateSine(true);
+  if (generateSine) changeGenerateSine(true);
   switch (loadt) {
     case 'Resistive':
       integrateCurrent = integrateResistorCurrent;
@@ -78,7 +55,6 @@ window.onCircuitType = loadt => {
   }
 };
 
-let previousTS: number = 0;
 const step: FrameRequestCallback = timeStamp => {
   const delta = (timeStamp - previousTS) / 1000; // calculating delta and converting it to seconds
 
@@ -102,10 +78,87 @@ const step: FrameRequestCallback = timeStamp => {
 };
 window.requestAnimationFrame(step);
 
-declare global {
-  interface Window {
-    onVoltage: (val: string) => void;
-    onCircuitType: (loadt: 'Resistive' | 'Inductive' | 'InductiveCorrected' | 'ResInd') => void;
-    onGenerateSine: (checked: boolean) => void;
+// Populating controls
+
+// Shortcut functions
+const br = () => document.createElement('br');
+
+// getting control element
+const controls = document.getElementById('controls')!;
+
+// Appending current
+const curEl = document.createElement('span');
+curEl.innerText = '0.0';
+controls.append('Current : ', curEl, 'A', br());
+
+// Appending current progress
+const curPgr = document.createElement('progress');
+curPgr.max = currentMax * 2;
+curPgr.value = 2;
+controls.append(curPgr, br());
+
+// Appending voltage
+const volEl = document.createElement('span');
+volEl.innerText = '0.0';
+const voltageLablel = document.createElement('label');
+voltageLablel.append('Voltage : ', volEl, 'V');
+voltageLablel.htmlFor = 'voltage';
+controls.append(voltageLablel, br());
+
+// Appending voltage input
+const voltageInp = document.createElement('input');
+voltageInp.id = 'voltage';
+voltageInp.type = 'range';
+voltageInp.max = voltageMax as unknown as string;
+voltageInp.min = -voltageMax as unknown as string;
+voltageInp.step = '0.2';
+voltageInp.value = '0';
+voltageInp.oninput = ev => (voltage = +(volEl.innerText = voltageInp.value));
+controls.append(voltageInp, br());
+
+// Appending generate sine
+const generateSineInp = document.createElement('input');
+generateSineInp.type = 'checkbox';
+generateSineInp.id = 'generateSine';
+generateSineInp.onchange = ev => changeGenerateSine(generateSineInp.checked);
+const generateSineInpLbl = document.createElement('label');
+generateSineInpLbl.htmlFor = 'generateSine';
+generateSineInpLbl.innerText = `Generate Sine Wave (${sineWaveFrequency}Hz)`;
+controls.append(generateSineInp, generateSineInpLbl, br());
+
+// Appending circuit type selector
+const circuitDiv = document.createElement('div');
+const circuitDivHeading = document.createElement('h4');
+circuitDivHeading.innerText = 'Select Circuit Type:';
+circuitDiv.append(circuitDivHeading);
+const handlers = {
+  'Pure Resistor': () => changeCircuitType('Resistive'),
+  'Pure Inductor': () => changeCircuitType('Inductive'),
+  'Pure Inductor (initial current corrected)': () => changeCircuitType('InductiveCorrected'),
+  'RL Series Circuit': () => changeCircuitType('ResInd'),
+};
+for (const text in handlers) {
+  if (Object.prototype.hasOwnProperty.call(handlers, text)) {
+    const inpt = document.createElement('input');
+    inpt.type = 'radio';
+    inpt.name = 'circuitType';
+    if (text === 'Pure Resistor') inpt.checked = true;
+    inpt.id = text;
+    inpt.onchange = (<any>handlers)[text];
+    circuitDiv.append(inpt);
+    const lbl = document.createElement('label');
+    lbl.innerText = text;
+    lbl.htmlFor = text;
+    circuitDiv.append(lbl, br());
   }
 }
+controls.append(circuitDiv);
+
+// Appending circuit constants
+const constantsDiv = document.createElement('div');
+const constantsDivHeading = document.createElement('h4');
+constantsDivHeading.innerText = 'Circuit Constants';
+constantsDiv.append(constantsDivHeading);
+constantsDiv.append(`Inductance : ${inductance}H`, br());
+constantsDiv.append(`Resistance : ${resistance}Ω`, br());
+controls.append(constantsDiv);
